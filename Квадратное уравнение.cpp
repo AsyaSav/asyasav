b59a5e77025b4ee5cbc4407094b8b53cd04e3e5e
Nastya <asyasavchenko087@gmail.com>
2021-10-10 21:59:36 +0300

    t

--- Квадратное уравнение.cpp
+++ Квадратное уравнение.cpp
@@ -5,11 +5,12 @@ using namespace std;
 
 int main()
 {
-    float x1, x2, a, b, c, D;
+    float x1, x2,  D;
+    int a, b, c;
     cin>> a>> b>> c;
     D = b * b - 4 * a * c;
     if (D >= 0) {
-        x1 = (-b + sqrt(D)) /(2 * a);
+        x1 = (-b + sqrt(D)) /( 2 * a);
         if (D == 0) {    
             cout << x1;
         }
