63f2701dcc9a383c2e7a22ad0e18680fba01917e
Nastya <asyasavchenko087@gmail.com>
2021-10-17 22:08:41 +0300

    ,

--- C++/Квадратное уравнение.cpp
+++ C++/Квадратное уравнение.cpp
@@ -5,18 +5,41 @@ using namespace std;
 
 int main()
 {
-    float x1, x2,  D;
-    int a, b, c;
-    cin>> a>> b>> c;
-    D = b * b - 4 * a * c;
-    if ((b * b - 4 * a * c) >= 0) {
-        x1 = (-b + sqrt(D)) /( 2 * a);
-        if ((b * b - 4 * a * c)== 0) {    
-            cout << x1;
-        }
-        else {
-        x2 = (-b - sqrt(D)) / (2 * a);
-        cout<<x1<<x2;
+    float x1, x2, D, a, b, c;
+    cin >> a >> b >> c;
+    if ((a == 0) and (b != 0) and (c != 0)) {
+        x1 = -c / b;
     }
+    if ((b == 0) and (a != 0) and (c != 0)) {
+        x1 = sqrt(-c / a);
+    }
+    if ((c == 0) and (b != 0) and (a != 0)) {
+        x1 = -b / a;
+        x2 = 0;
+    }
+    if ((b == 0) and (c == 0) and (a != 0)) {
+        x1 = 0;
+    }
+    if ((b == 0) and (c == 0) and (a == 0)) {
+        cout << "x может принять любое значение ";
+    }
+    if ((b == 0) and (c != 0) and (a == 0)) {
+        cout << "решений нет";
+    }
+    if ((b != 0) and (c == 0) and (a == 0)) {
+        x1 = 0;
+    }
+    if ((b != 0) and (c != 0) and (a != 0)) {
+        D = b * b - 4 * a * c;
+        if (D >= 0) {
+            x1 = (-b + sqrt(D)) / (2 * a);
+            if (D == 0) {
+                cout << x1;
+            }
+            else {
+                x2 = ((-b - sqrt(D))) / (2 * a);
+                cout << x1 << x2;
+            }
+        }
     }
 }
